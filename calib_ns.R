484444d5db570654cd53f218dcd95e2046f8c87a
smquinn99 <smquinn@mun.ca>
2025-07-14 12:06:17 -0230

    july14th_testrun

---
+++ calib_ns.R
@@ -0,0 +1,80 @@
+#this takes you from the model all the way to getting the calibration values
+#along the way we are going to check our training data across several different graphs to determine whether or not we actually need to 
+#calibrate our data, or if it's okay on it's own. In our case, the SD could benefit from some calibration, although it's not far off
+library(dplyr)
+library(randomForest)
+library(tidyverse)
+library(terra)
+
+#Split the data into test and train sets
+k<-10                                    
+fold<-rep(1:k, length.out= nrow(till_df))  
+fold<-sample(fold)                         
+till_df$fold<-fold
+for (i in 1:k) {    
+  print(i)  
+  test<- till_df[till_df$fold==i, ] 
+  train<- till_df[till_df$fold!=i, ]}
+
+#predict uncertainty for the dataset
+p<-predict(rf_nonspatial_alldata, train, predict.all=TRUE) 
+pmean<- apply(p$individual, MARGIN=1, mean, na.rm= TRUE)
+psd<- apply(p$individual, MARGIN=1, sd, na.rm= TRUE)
+
+ind<-data.frame(observed=train$Al_avail, predicted=p, mean=pmean, sd=psd) 
+ind$error<- (ind$observed- ind$predicted.aggregate)
+
+#Plot the r value, that is check the distribution of the error/predicted sd
+ind<- ind %>% mutate(r = error/sd)
+r<- ind$r
+mean_Rstat<- mean(r)
+ssd_Rstat<-sd(r)
+
+hist(r, breaks=25, freq=FALSE,
+     xlab="r statistic", 
+     main="",  col="grey",
+     ylim=c(0, 1),
+     xlim=c(-4, 4))
+curve(dnorm(x, mean=0, sd=1), 
+      col="red", lwd=2, add=TRUE, yaxt="n")
+
+#Bin the values and check their averaged RMSE against their average SD
+ind<- ind %>% 
+  ungroup() %>%
+  mutate(sd_bin = cut(sd, 15))
+
+Stats <- ind %>% group_by(sd_bin) %>% 
+  summarize(mean_sd_group = mean(sd), RMS_group = RMSE(observed, predicted.aggregate), NumFrames = n())
+
+# Plot RMSE residuals vs standard deviation of the bootstrapped estimates
+model<- lm(Stats$mean_sd_group~Stats$RMS_group)
+summary(model)
+plot( Stats$mean_sd_group, Stats$RMS_group, 
+     main= " RMS Residuals vs Mean Binned SD (Aspatial)",
+     xlab = "Mean Binned SD", 
+     ylab = "RMS Residuals",
+     abline(a=0, b=1, col= "RED"),
+    
+     xlim=c(0, 1.5),
+     ylim=c(0, 1.5)
+)
+
+
+#Calculate calibration factors to scale the data with
+#First, make log likelihood function
+lfun<- function(x, sd, r) {                   
+  x1 <- x[1]                                 
+  x2 <- x[2]                                 
+  (sum(log(2*pi) + log((x1*sd+x2)^2) + ((r^2)/((x1*sd+x2)^2)))) 
+}
+
+##Next, optimize NLL
+optimization<-optim(c(0.6, 0.21010965), lfun,  sd=ind$sd, r=ind$error, method = "BFGS")
+optimization
+oparams<- optimization$par
+a<-oparams[1]
+b<-oparams[2]
+
+
+
+
---
+++ ns_final_validation_eqfreq.R
@@ -0,0 +1,113 @@
+
+library(dplyr)
+#rf_nonspatial_alldata<- randomForest(Al_avail~ ruggedness + bedrockgeo + unit_size + NLDEM + climate_zones_RS + distOcean_RS + slope+ surficial_regional_RS, till_df, ntree=1000, mtry=4)
+
+p4<-predict(rf_nonspatial_alldata, till_df, predict.all=TRUE) 
+p4mean<- apply(p4$individual, MARGIN=1, mean, na.rm= TRUE)
+p4sd<- apply(p4$individual, MARGIN=1, sd, na.rm= TRUE)
+ind4$error<- (ind4$observed- ind4$predicted.aggregate)
+
+#add my calibrated values
+ind4<- ind4 %>% mutate(cal = (sd*a+b))
+#r value plot 
+#first make the r statistic (r = residual/sd) 
+ind4<- ind4 %>% mutate(rC = error/cal)
+rC<- ind4$rC
+ind4<- ind4 %>% mutate(rUC = error/sd)
+rUC<-ind4$rUC
+
+#calculate the mean and standard deviations
+mean_Rstat_C<- mean(ind4$rC)
+sd_Rstat_C<-sd(ind4$rC)
+
+mean_Rstat_UC<- mean(ind4$rUC)
+sd_Rstat_UC<-sd(ind4$rUC)
+
+uncal_RMSE<- RMSE(ind4$error, ind4$sd)
+Cal_RMSE<-RMSE(ind4$error, ind4$cal)
+#Now plot
+
+png( filename = "D:/GIS_SCOUT/GIS_SCOUT/figures/cal_asp_hist.png", width = 3.25, height = 3, units = "in", res = 200)
+par(mar= c(3, 3, 1, 1))
+hist(rUC, breaks=25, freq=FALSE, cex.main = 1,
+     xlab="", cex.lab=0.8, cex.axis=0.8,
+     main="",  col="grey",
+     ylim=c(0, 1.5),
+     xlim=c(-4, 4))
+hist(rC, breaks=50, freq=FALSE,
+     xlab="", cex = 0.8,
+     main="", add=TRUE, col=rgb(0,0,1,0.4),
+     ylim=c(0, 1),
+     xlim=c(-4, 4))
+curve(dnorm(x, mean=0, sd=1), 
+      col="red", lwd=2, add=TRUE, yaxt="n")
+title(xlab= "residuals/sd", ylab = "Density", line = 2, cex.lab = "0.8")
+legend("topright", c("Uncalibrated", "Calibrated", "Normal Distribution"), cex = 0.8,
+       pch = c(15, 15, NA), lty = c(NA, NA, 1), col =c("grey", rgb(0, 0, 1, 0.4), "red"))
+dev.off()
+
+#make an RMSE function
+RMSE = function(observed_data, predicted_data){
+  sqrt(mean((observed_data - predicted_data)^2))
+}
+
+#binning
+quantile(ind4$sd)
+ind4<- ind4 %>% 
+  ungroup() %>%
+  mutate(sd_bin = cut_number(ind4$sd, n = 30))
+#Obtain RMSE of the bin for uncalibrated values
+Stats6 <- ind4 %>% group_by(sd_bin) %>% 
+  summarize(mean_sd_group = mean(sd), RMS_group_UC = RMSE(observed, predicted.aggregate), NumFramesUC = n())
+
+# do the same thing for the calibrated values
+ind4<- ind4 %>% 
+  ungroup() %>%
+  mutate(sdcal_bin = cut_number(ind4$cal, n = 30))
+#now obtain the RMSE of calibrated bins
+Stats7 <- ind4 %>% group_by(sdcal_bin) %>% 
+  summarize(mean_sd_group = mean(sd), mean_sdcal_group = mean(cal), RMS_group = RMSE(observed, predicted.aggregate), NumFramesC = n())
+
+#Identify bins with few values in them
+lowcountsC<- subset(Stats7, NumFramesC<30)
+adequate_countsC<- subset(Stats7, NumFramesC>30)
+
+lowcountsUC<- subset(Stats6, NumFramesUC<30)
+adequate_countsUC<- subset(Stats6, NumFramesUC>30)
+
+#now make a plot (RMSE residuals vs standard deviation of the bootstrapped estimates)
+png( filename = "D:/GIS_SCOUT/GIS_SCOUT/figures/cal_asp_scattercode_equalsize.png", width = 3.25, height = 2.5, units = "in", res = 200)
+par(mar= c(3, 3, 0, 1))
+plot(adequate_countsC$mean_sdcal_group, adequate_countsC$RMS_group, 
+     cex.lab=0.7, cex.axis=0.6, cex.main= 0.2,
+     xlab = "", 
+     ylab = "",
+     abline(a=0, b=1, col= "red"),
+     pch = 21, bg = "blue", col = "blue",
+     xlim=c(0, 0.6),
+     ylim=c(0, .6))
+points(adequate_countsC$mean_sdcal_group, adequate_countsC$RMS_group,pch = 21, bg = "blue", col = "blue")
+points( adequate_countsUC$mean_sd_group, adequate_countsUC$RMS_group_UC,pch = 21, bg = "grey", col = "grey" )
+points( lowcountsC$mean_sdcal_group, lowcountsC$RMS_group, col="blue")
+points( lowcountsUC$mean_sd_group, lowcountsUC$RMS_group_UC, col="grey")
+title(xlab= "Mean Binned SD", ylab = "RMS Residuals", line = 2, cex.lab = "0.7")
+legend("bottomright", c("Uncalibrated", "Calibrated", "Low Counts", "Identity Function"), cex = 0.7, 
+       pch = c(16, 16, 1, NA), lty = c(NA, NA, NA, 1), col =c("grey", "blue", "black", "red"))
+dev.off()
+
+
+#plot histogram of bins
+png( filename = "D:/GIS_SCOUT/GIS_SCOUT/figures/cal_asp_bins_eqfreq.png", width = 3.25, height = 1.25, units = "in", res = 200)
+par(mar= c(0, 3, 1, 1))
+hist(ind4$sd, breaks=50,  cex.lab=0.7, cex.axis=0.7, cex.main= 0.7, xaxt= "n",
+     xlab=" ", 
+     main="", col="grey" ,
+     ylab= "",
+     ylim=c(0, 10000),
+     xlim=c(0, 0.6))
+hist(ind4$cal, breaks=35,  add=TRUE, col=rgb(0,0,1,0.4),,
+     ylim=c(0, 10000),
+     xlim=c(0, 1.5))
+legend("topright", c("Uncalibrated", "Calibrated"), cex = 0.8, fill =c("grey", rgb(0,0,1,0.4)))
+title( ylab = "Bin Counts", line = 2, cex.lab = "0.7")
+dev.off()
